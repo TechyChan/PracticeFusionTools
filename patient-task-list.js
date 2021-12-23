0ea2a3ecaf8ad840c96789260e72acaa1be9e668
David Ding <dingo9082@hotmail.com>
2021-12-23 15:14:07 -0500

    increment version

--- patient-task-list.js
+++ patient-task-list.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Practice Fusion Patient Task List
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @include      /^https?://.*practicefusion\.com/.*$/
